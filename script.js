a2d8d608f3f5fa144cdc79b2a5bf20d5b0b818a8
kevinxmole <kevinxiemole@gmail.com>
2024-08-19 12:33:18 +1000

    test

---
+++ script.js
@@ -0,0 +1,34 @@
+// function add(a ,b) {
+//     return a + b
+    
+// }
+
+// console.log(add(5, 6));
+// console.log(add(5, 6));
+// console.log(add(5, 6));
+// console.log(add(5, 6));
+// console.log(add(5, 6));
+
+// x = [1, "two", true]
+
+// console.log(x[1])
+
+// const employee = {
+//     name: "James",
+//     address: "6 Brown Street, Chatswood",
+//     phone: 488568877,
+//     department: "IT"
+// }
+
+// console.log(employee.name)
+// console.log(employee.address)
+// console.log(employee.phone)
+// console.log(employee.department)
+
+let l = require('lodash')
+
+let a = "this is a test";
+
+let b = l.toUpper(a);
+
+console.log(b);
